8dd67725e6fa9e182a0f2405c04c7aadafaf018e
Zhang Wei <weeizhang@outlook.com>
2016-08-05 17:18:10 +0800

    add filter and add case menue in tool bar

--- app/src/main/java/none/healthaide/MainActivity.java
+++ app/src/main/java/none/healthaide/MainActivity.java
@@ -2,6 +2,9 @@ package none.healthaide;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_new_case) {
+            //TODO: show add new case page
+            return true;
+        } else if (item.getItemId() == R.id.action_filter_case) {
+            //TODO: show search
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
