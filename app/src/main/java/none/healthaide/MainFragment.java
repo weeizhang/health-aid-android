056056944b93930f8858237d114eed44c2ceb5a0
Zhang Wei <weeizhang@outlook.com>
2016-08-08 17:08:26 +0800

    add main fragment

---
+++ app/src/main/java/none/healthaide/MainFragment.java
@@ -0,0 +1,16 @@
+package none.healthaide;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
